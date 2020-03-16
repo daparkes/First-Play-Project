66dd9454d21ee9c0fd8d9cf27c88cd4f7b355a4d
Dan Parkes <dan.parkes15@gmail.com>
2020-03-16 17:26:45 +0000

    exercises

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -6,8 +6,51 @@ import play.api.mvc._
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def index: Action[AnyContent] = Action {
+    Ok(views.html.index("Hello World!"))
   }
 
+  def help: Action[AnyContent] = Action {
+    Redirect("https://www.google.com/")
+  }
+
+  def infinite: Action[AnyContent] = Action {
+    Redirect("/infinite")
+  }
+  
+  def todo: Action[AnyContent] = TODO
+
+  def hello(): Action[AnyContent] = Action {
+    implicit request: Request[AnyContent] =>
+    implicit val MyCustomCharset: Codec = Codec.javaSupported(
+      "iso-8859-1")
+    Ok(request.session.get("connected").getOrElse("User is not logged in"))
+
+  }
+
+  def helloHTML:Action[AnyContent] = Action {
+    Ok(<h1>Hello World!</h1>).as(HTML)
+  }
+  def helloTextHTML: Action[AnyContent] = Action {
+    Ok(<h1>Hello World!</h1>).as("text/html")
+  }
+
+  def cookies:Action[AnyContent] = Action {
+    Ok("Hello World!").withCookies(
+      Cookie("colour", "blue")
+    )
+    implicit request: Request[AnyContent] =>
+      request.cookies.get("colour") match {
+        case Some(cookie) => Ok(s"Your cookie value is: ${cookie.value}")
+        case _ => Ok("Cookie not found")
+      }
+  }
+
+  def read() = Action { implicit request: Request[AnyContent] =>
+    Ok(request.flash.get("success").getOrElse("Something went wrong while redirecting"))
+  }
+
+  def write() = Action {
+    Redirect("/read").flashing("success" -> "You have been successfully redirected.")
+  }
 }
